76cbc370f09764263d4313ed4dc94d3d9ac91e81
ishant <ishantbhoyar59@gmail.com>
2025-07-26 17:56:51 +0530

    utils updated

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,32 @@
+const asyncHandler = (requestHandler) => (req, res, next) => {
+  Promise.resolve(requestHandler(req, res, next)).catch(next);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const asyncHandler =(controller) => async (req, res, next) => {
+//     try {
+//         await controller(req, res, next)
+
+//     } catch (error) {
+
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message})
+        
+//     }
+// }
+
+export {asyncHandler}
